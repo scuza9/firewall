62d7855b96ac4f8bb5ccf02877d08065f3801eba
chrisNolas <nocch83@gmail.com>
2021-08-25 21:10:13 -0500

    Se agrega nueva función para obtener ips vivas

--- arpSpoofer/index.js
+++ arpSpoofer/index.js
@@ -3,31 +3,35 @@ const arp = require('arpjs')
 const fs = require('fs')
 const local = require('ipmask')()
 const address = require('address')
+const Arpping = require('arpping');
+const arpping = new Arpping();
 
 address.dns(function (err, addrs) {
-let infoLocal = { 
-    ipLocal: local.address,
-    dnsLocal: addrs,
-    info: ip.cidrSubnet(local.cidr)
-};
- 
-let data = JSON.stringify(infoLocal, null, 2)
+  let infoLocal = { 
+      ipLocal: local.address,
+      dnsLocal: addrs,
+      info: ip.cidrSubnet(local.cidr)
+  };
+  
+  let data = JSON.stringify(infoLocal, null, 2)
 
-fs.writeFile('infoLocal.log', data, err => {
-  if (err) {
-    console.error(err)
-    return
-  }
-})
+  fs.writeFile('infoLocal.log', data, err => {
+    if (err) {
+      console.error(err)
+      return
+    }
+  })
 
-arp.table(function (err, table) {
-    fs.writeFile('arpLocal.log', JSON.stringify(table, null, 2), err => {
+}); 
+
+arpping.discover()
+    .then(hosts => {
+      fs.writeFile('subNetLocal.log', JSON.stringify(hosts, null, 4), err => {
         if (err) {
           console.error(err)
           return
         }
-      })
-})
-});
-
+        process.exit();
+      }) 
+    }).catch(err => console.log(err));
 
